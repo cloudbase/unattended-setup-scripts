4c190cb780dfbd8922c8b6ca9b48760416b9d8fb
Alessandro Pilotti <ap@pilotti.it>
2013-09-22 02:32:46 +0300

    Adds Hyper-V 2012 R2 template creation script

---
+++ create-hyperv-2012-r2-vm-template.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+./create-vm-template.sh datastore3 winhyperv hyperv-2012-template 2048 20G /vmfs/volumes/datastore2/iso/en_microsoft_hyper-v_server_2012_r2_x64_dvd_2708236.iso windows unattend.flp "VM Network" 
+
