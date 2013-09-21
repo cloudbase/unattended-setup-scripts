8a2e51b38c22eaa0ae0d7fb9d72db9ca7d1a7331
Alessandro Pilotti <ap@pilotti.it>
2013-09-22 00:10:42 +0300

    Updates DeployTools.ps1

--- DeployTools.ps1
+++ DeployTools.ps1
@@ -6,3 +6,22 @@ $filename = "7z922-x64.msi"
 Invoke-WebRequest -uri "http://freefr.dl.sourceforge.net/project/sevenzip/7-Zip/9.22/7z922-x64.msi" -outFile $filename
 Start-Process -Wait msiexec.exe -ArgumentList "/i $filename /qn"
 del $filename
+
+$filename = "npp.6.4.5.Installer.exe"
+Invoke-WebRequest -uri "http://download.tuxfamily.org/notepadplus/6.4.5/npp.6.4.5.Installer.exe" -outFile $filename
+Start-Process -Wait -FilePath $filename -ArgumentList "/S"
+del $filename
+setx /m PATH "$ENV:PATH;$ENV:ProgramFiles (x86)\Notepad++"
+
+Invoke-WebRequest -uri "https://dl.dropboxusercontent.com/u/9060190/wget.exe" -OutFile "wget.exe"
+Invoke-WebRequest -uri  "http://the.earth.li/~sgtatham/putty/latest/x86/putty.exe" -OutFile "putty.exe"
+Invoke-WebRequest -uri  "http://the.earth.li/~sgtatham/putty/latest/x86/pscp.exe" -OutFile "pscp.exe"
+
+$modules_dir = "$ENV:USERPROFILE\Documents\WindowsPowerShell\Modules"
+mkdir $modules_dir
+
+$filename = PSWindowsUpdate.zip
+Invoke-WebRequest -Uri "http://gallery.technet.microsoft.com/scriptcenter/2d191bcd-3308-4edd-9de2-88dff796b0bc/file/41459/25/PSWindowsUpdate.zip" -OutFile $filename
+cmd /c "C:\Program Files\7-Zip\7z.exe" x -o%USERPROFILE%\Documents\WindowsPowerShell\Modules $filename
+del $filename
+
