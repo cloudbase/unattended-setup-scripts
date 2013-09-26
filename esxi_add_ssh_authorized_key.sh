1c53c58e07f0ba5f3803a6b6e6aad4de2b52b522
Alessandro Pilotti <ap@pilotti.it>
2013-09-26 20:42:02 +0200

    Adds script to set up passwordless SSH on ESXi

---
+++ esxi_add_ssh_authorized_key.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Add your user's pub key to an ESXi host authorized keys
+# During the process you'll be prompted for the remote password twice
+
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <username> <esxi_host> [<pub_key_path>]"
+    exit 1
+fi
+
+USERNAME=$1
+ESXI_HOST=$2
+
+if [ $# -eq 3 ]; then
+    PUB_KEY=$3
+else
+    PUB_KEY=~/.ssh/id_rsa.pub
+fi
+
+REMOTE_TMP_PUB_KEY=/tmp/ssh_pub_$USERNAME.pub
+
+scp $PUB_KEY $USERNAME@$ESXI_HOST:$REMOTE_TMP_PUB_KEY
+ssh $USERNAME@$ESXI_HOST "mkdir -p /etc/ssh/keys-$USERNAME && cat $REMOTE_TMP_PUB_KEY >> /etc/ssh/keys-$USERNAME/authorized_keys && rm $REMOTE_TMP_PUB_KEY"
+
