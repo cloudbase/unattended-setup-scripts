285e339cf65f49296c84ea1f17113c2faaced846
Alessandro Pilotti <ap@pilotti.it>
2013-09-27 02:26:27 +0200

    Renamed unattend.flp

--- create-hyperv-2012-vm-template.sh
+++ create-hyperv-2012-vm-template.sh
@@ -2,7 +2,7 @@
 
 BASEDIR=$(dirname $0)
 DATASTORE=datastore1
-FLOPPY_IMAGE_PATH=$BASEDIR/unattend.flp
+FLOPPY_IMAGE_PATH=$BASEDIR/unattend_hyperv_2012.flp
 
 $BASEDIR/create-vm-template.sh $DATASTORE winhyperv hyperv-2012-template 2048 20G /vmfs/volumes/datastore2/iso/en_microsoft_hyper-v_server_2012_x64_dvd_915600.iso windows $FLOPPY_IMAGE_PATH "VM Network" 
 
