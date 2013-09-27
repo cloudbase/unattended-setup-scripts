56acd9de8ea5b528efde400366dbf9578c615f9c
Alessandro Pilotti <ap@pilotti.it>
2013-09-27 03:50:20 +0200

    Adds cloud-init support in CentOS image

---
+++ create-centos-64-cloudinit-vm-template.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+BASEDIR=$(dirname $0)
+
+FLOPPY_IMAGE_PATH=$BASEDIR/ks_cloudinit.flp
+DATASTORE=datastore1
+
+$BASEDIR/create-vm-template.sh $DATASTORE rhel6-64 centos-6.4-cloudinit-template 1024 10G /vmfs/volumes/datastore2/iso/CentOS-6.4-x86_64-bin-DVD1.iso linux $FLOPPY_IMAGE_PATH "VM Network" 
+
