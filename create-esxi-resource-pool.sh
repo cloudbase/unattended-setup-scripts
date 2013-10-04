ac2cfe762fb3964000598cba0c44216eb8a9809b
Alessandro Pilotti <ap@pilotti.it>
2013-10-04 21:59:24 +0300

    Set default options when creating ESXi resource pool

--- create-esxi-resource-pool.sh
+++ create-esxi-resource-pool.sh
@@ -7,6 +7,6 @@ fi
 
 POOL_NAME=$1
 
-POOL_ID=`vim-cmd hostsvc/rsrc/create ha-root-pool $POOL_NAME | sed -rn "s/'vim.ResourcePool:(.+)'/\1/p"`
+POOL_ID=`vim-cmd hostsvc/rsrc/create --cpu-min-expandable=true --cpu-shares=normal --mem-min-expandable=true --mem-shares=normal ha-root-pool $POOL_NAME | sed -rn "s/'vim.ResourcePool:(.+)'/\1/p"`
 echo $POOL_ID
 
