2896e9f67b747d23f7820834ad4b259940cfc16c
Alessandro Pilotti <ap@pilotti.it>
2013-10-04 18:42:51 +0300

    Added grace period to poweroff script

--- esxi-power-off-vms-without-tools.sh
+++ esxi-power-off-vms-without-tools.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -e
 
+GRACE_PERIOD=3600
+
 for VMID in `vim-cmd vmsvc/getallvms | awk '{ if (NR > 1) print $1 }'`
 do
     vim-cmd vmsvc/power.getstate $VMID
@@ -9,8 +11,11 @@ do
     if [[ -n "$ON" ]]; then
         HB=`vim-cmd vmsvc/get.guestheartbeatStatus $VMID`
         if [ "$HB" != "green" ]; then
-            echo "Powering off VM: $VMID"
-            /bin/vim-cmd vmsvc/power.off $VMID
+            UPTIME=`vim-cmd vmsvc/get.summary 259 | sed -rn 's/\ +uptimeSeconds\ =\ ([0-9]+),\ +/\1/p'`
+            if [ "$UPTIME" -gt "$GRACE_PERIOD" ]; then
+                echo "Powering off VM: $VMID"
+                vim-cmd vmsvc/power.off $VMID
+            fi
         fi
     fi
 done
