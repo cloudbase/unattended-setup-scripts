71963bbe9dc26bae57a7eae83597c59682f3ed55
Alessandro Pilotti <ap@pilotti.it>
2013-10-04 15:54:43 +0300

    Resource pool support

--- create-vm-template.sh
+++ create-vm-template.sh
@@ -1,3 +1,6 @@
+#!/bin/sh
+set -e
+
 DATASTORE=$1
 ISO_PATH=$6
 VM_NAME=$3
@@ -8,19 +11,14 @@ VMDK_SIZE=$5
 VMWARE_TOOLS_ISO=$7
 VM_NETWORK=$9
 
+POOL_NAME=templates
+
 DATASTORE_PATH=/vmfs/volumes/$DATASTORE
 
 BASEDIR=$(dirname $0)
 
-VMID=`/bin/vim-cmd vmsvc/getallvms | awk -vvmname="$VM_NAME" '{if ($2 == vmname) print $1}'`
-if [[ -n "$VMID" ]]; then
-    OFF=`vim-cmd vmsvc/power.getstate $VMID | grep "Powered off"`
-    if [[ -z "$OFF" ]]; then
-        /bin/vim-cmd vmsvc/power.off $VMID
-    fi
-    /bin/vim-cmd vmsvc/unregister $VMID
-fi
+$BASEDIR/delete-esxi-vm.sh "$VM_NAME" "$DATASTORE"
 
-rm -rf $DATASTORE_PATH/$VM_NAME
-$BASEDIR/create-esxi-vm.sh $DATASTORE $GUEST_OS $VM_NAME $RAM 2 2 $VMDK_SIZE - $ISO_PATH $VMWARE_TOOLS_ISO $FLOPPY_PATH true "$VM_NETWORK"
+echo "Creating VM"
+$BASEDIR/create-esxi-vm.sh $DATASTORE $GUEST_OS $VM_NAME $POOL_NAME $RAM 2 2 $VMDK_SIZE - $ISO_PATH $VMWARE_TOOLS_ISO $FLOPPY_PATH true "$VM_NETWORK"
 
