a06ad9f46bf23ac44a7f26c5e8dc206083c9537e
Alessandro Pilotti <ap@pilotti.it>
2013-10-04 21:48:45 +0300

    RDO deploy updates

--- deploy-rdo-vms.sh
+++ deploy-rdo-vms.sh
@@ -30,18 +30,42 @@ NETWORK_VM_NAME="$RDO_NAME"_network
 QEMU_COMPUTE_VM_NAME="$RDO_NAME"_compute_qemu
 HYPERV_COMPUTE_VM_NAME="$RDO_NAME"_compute_hyperv
 
-$BASEDIR/create-esxi-resource-pool.sh $POOL_NAME
+POOL_ID=`$BASEDIR/get-esxi-resource-pool-id.sh $POOL_NAME`
+if [ -z $POOL_ID ]; then
+    $BASEDIR/create-esxi-resource-pool.sh $POOL_NAME
+fi
 
-/bin/vim-cmd hostsvc/net/portgroup_add $EXT_SWITCH $EXT_NETWORK
-/bin/vim-cmd hostsvc/net/portgroup_set --nicorderpolicy-active=$VMNIC $EXT_SWITCH $EXT_NETWORK
+PORTGROUP_EXISTS=`$BASEDIR/check-esxi-portgroup-exists.sh "$EXT_NETWORK"`
+if [ -z "$PORTGROUP_EXISTS" ]; then
+    /bin/vim-cmd hostsvc/net/portgroup_add "$EXT_SWITCH" "$EXT_NETWORK"
+fi
+/bin/vim-cmd hostsvc/net/portgroup_set --nicorderpolicy-active=$VMNIC "$EXT_SWITCH" "$EXT_NETWORK"
 
-/bin/vim-cmd hostsvc/net/portgroup_add $EXT_SWITCH $MGMT_NETWORK
-/bin/vim-cmd hostsvc/net/portgroup_set --nicorderpolicy-active=$VMNIC $EXT_SWITCH $MGMT_NETWORK
+PORTGROUP_EXISTS=`$BASEDIR/check-esxi-portgroup-exists.sh "$MGMT_NETWORK"`
+if [ -z "$PORTGROUP_EXISTS" ]; then
+    /bin/vim-cmd hostsvc/net/portgroup_add "$EXT_SWITCH" "$MGMT_NETWORK"
+fi
+/bin/vim-cmd hostsvc/net/portgroup_set --nicorderpolicy-active=$VMNIC "$EXT_SWITCH" "$MGMT_NETWORK"
 
-$BASEDIR/create-esxi-switch.sh $DATA_NETWORK
+SWITCH_EXISTS=`$BASEDIR/check-esxi-switch-exists.sh "$DATA_NETWORK"`
+if [ -z "$SWITCH_EXISTS" ]; then
+    $BASEDIR/create-esxi-switch.sh "$DATA_NETWORK"
+fi
 
-$BASEDIR/create-esxi-vm.sh $DATASTORE $LINUX_GUEST_OS $CONTROLLER_VM_NAME $POOL_NAME 1024 2 2 11G $LINUX_TEMPLATE - - - true "$MGMT_NETWORK"
-$BASEDIR/create-esxi-vm.sh $DATASTORE $LINUX_GUEST_OS $NETWORK_VM_NAME $POOL_NAME 1024 2 2 11G $LINUX_TEMPLATE - - - true "$MGMT_NETWORK" "$DATA_NETWORK" "$EXT_NETWORK"
-$BASEDIR/create-esxi-vm.sh $DATASTORE $LINUX_GUEST_OS $QEMU_COMPUTE_VM_NAME $POOL_NAME 4096 2 2 30G $LINUX_TEMPLATE - - - true "$MGMT_NETWORK" "$DATA_NETWORK"
-$BASEDIR/create-esxi-vm.sh $DATASTORE $HYPERV_GUEST_OS $HYPERV_COMPUTE_VM_NAME $POOL_NAME 4096 2 2 60G $HYPERV_TEMPLATE - - - true "$MGMT_NETWORK" "$DATA_NETWORK" 
+$BASEDIR/delete-esxi-vm.sh "$CONTROLLER_VM_NAME" $DATASTORE
+$BASEDIR/delete-esxi-vm.sh "$NETWORK_VM_NAME" $DATASTORE
+$BASEDIR/delete-esxi-vm.sh "$QEMU_COMPUTE_VM_NAME" $DATASTORE
+$BASEDIR/delete-esxi-vm.sh "$HYPERV_COMPUTE_VM_NAME" $DATASTORE
+
+$BASEDIR/create-esxi-vm.sh $DATASTORE $LINUX_GUEST_OS $CONTROLLER_VM_NAME $POOL_NAME 1024 2 2 11G $LINUX_TEMPLATE - - - false "$MGMT_NETWORK"
+$BASEDIR/create-esxi-vm.sh $DATASTORE $LINUX_GUEST_OS $NETWORK_VM_NAME $POOL_NAME 1024 2 2 11G $LINUX_TEMPLATE - - - false "$MGMT_NETWORK" "$DATA_NETWORK" "$EXT_NETWORK"
+$BASEDIR/create-esxi-vm.sh $DATASTORE $LINUX_GUEST_OS $QEMU_COMPUTE_VM_NAME $POOL_NAME 4096 2 2 30G $LINUX_TEMPLATE - - - false "$MGMT_NETWORK" "$DATA_NETWORK"
+$BASEDIR/create-esxi-vm.sh $DATASTORE $HYPERV_GUEST_OS $HYPERV_COMPUTE_VM_NAME $POOL_NAME 4096 2 2 60G $HYPERV_TEMPLATE - - - false "$MGMT_NETWORK" "$DATA_NETWORK" 
+
+#sleep 20
+
+$BASEDIR/power-on-esxi-vm.sh "$CONTROLLER_VM_NAME"
+$BASEDIR/power-on-esxi-vm.sh "$NETWORK_VM_NAME"
+$BASEDIR/power-on-esxi-vm.sh "$QEMU_COMPUTE_VM_NAME"
+$BASEDIR/power-on-esxi-vm.sh "$HYPERV_COMPUTE_VM_NAME"
 
