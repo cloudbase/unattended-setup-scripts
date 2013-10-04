e1ca9883813286a7c9064f78d4fbc6b350d703ed
Alessandro Pilotti <ap@pilotti.it>
2013-10-04 15:55:22 +0300

    RDO deployment updates

--- deploy-rdo-vms.sh
+++ deploy-rdo-vms.sh
@@ -17,16 +17,20 @@ MGMT_NETWORK="$RDO_NAME"_mgmt
 DATA_NETWORK="$RDO_NAME"_data
 EXT_NETWORK="$RDO_NAME"_external
 
+POOL_NAME=$RDO_NAME
+
 LINUX_GUEST_OS=rhel6-64
 HYPERV_GUEST_OS=winhyperv
 
-LINUX_TEMPLATE=/vmfs/volumes/datastore2/centos-6.4-template/centos-6.4-template.vmdk
-HYPERV_TEMPLATE=/vmfs/volumes/datastore2/hyperv-2012-template/hyperv-2012-template.vmdk
+LINUX_TEMPLATE=/vmfs/volumes/datastore1/centos-6.4-template/centos-6.4-template.vmdk
+HYPERV_TEMPLATE=/vmfs/volumes/datastore1/hyperv-2012-template/hyperv-2012-template.vmdk
 
 CONTROLLER_VM_NAME="$RDO_NAME"_controller
 NETWORK_VM_NAME="$RDO_NAME"_network
 QEMU_COMPUTE_VM_NAME="$RDO_NAME"_compute_qemu
 HYPERV_COMPUTE_VM_NAME="$RDO_NAME"_compute_hyperv
+
+$BASEDIR/create-esxi-resource-pool.sh $POOL_NAME
 
 /bin/vim-cmd hostsvc/net/portgroup_add $EXT_SWITCH $EXT_NETWORK
 /bin/vim-cmd hostsvc/net/portgroup_set --nicorderpolicy-active=$VMNIC $EXT_SWITCH $EXT_NETWORK
@@ -36,8 +40,8 @@ HYPERV_COMPUTE_VM_NAME="$RDO_NAME"_compute_hyperv
 
 $BASEDIR/create-esxi-switch.sh $DATA_NETWORK
 
-$BASEDIR/create-esxi-vm.sh $DATASTORE $LINUX_GUEST_OS $CONTROLLER_VM_NAME 1024 2 2 11G $LINUX_TEMPLATE - - - true "$MGMT_NETWORK"
-$BASEDIR/create-esxi-vm.sh $DATASTORE $LINUX_GUEST_OS $NETWORK_VM_NAME 1024 2 2 11G $LINUX_TEMPLATE - - - true "$MGMT_NETWORK" "$DATA_NETWORK" "$EXT_NETWORK"
-$BASEDIR/create-esxi-vm.sh $DATASTORE $LINUX_GUEST_OS $QEMU_COMPUTE_VM_NAME 4096 2 2 30G $LINUX_TEMPLATE - - - true "$MGMT_NETWORK" "$DATA_NETWORK"
-$BASEDIR/create-esxi-vm.sh $DATASTORE $HYPERV_GUEST_OS $HYPERV_COMPUTE_VM_NAME 4096 2 2 60G $HYPERV_TEMPLATE - - - true "$MGMT_NETWORK" "$DATA_NETWORK" 
+$BASEDIR/create-esxi-vm.sh $DATASTORE $LINUX_GUEST_OS $CONTROLLER_VM_NAME $POOL_NAME 1024 2 2 11G $LINUX_TEMPLATE - - - true "$MGMT_NETWORK"
+$BASEDIR/create-esxi-vm.sh $DATASTORE $LINUX_GUEST_OS $NETWORK_VM_NAME $POOL_NAME 1024 2 2 11G $LINUX_TEMPLATE - - - true "$MGMT_NETWORK" "$DATA_NETWORK" "$EXT_NETWORK"
+$BASEDIR/create-esxi-vm.sh $DATASTORE $LINUX_GUEST_OS $QEMU_COMPUTE_VM_NAME $POOL_NAME 4096 2 2 30G $LINUX_TEMPLATE - - - true "$MGMT_NETWORK" "$DATA_NETWORK"
+$BASEDIR/create-esxi-vm.sh $DATASTORE $HYPERV_GUEST_OS $HYPERV_COMPUTE_VM_NAME $POOL_NAME 4096 2 2 60G $HYPERV_TEMPLATE - - - true "$MGMT_NETWORK" "$DATA_NETWORK" 
 
