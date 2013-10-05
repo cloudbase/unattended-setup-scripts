bc23387a7315b22a89682190be7f5aaab0ed1305
Alessandro Pilotti <ap@pilotti.it>
2013-10-05 21:59:06 +0300

    Bug fixes

--- esxi-power-off-vms-without-tools.sh
+++ esxi-power-off-vms-without-tools.sh
@@ -5,16 +5,14 @@ GRACE_PERIOD=3600
 
 for VMID in `vim-cmd vmsvc/getallvms | awk '{ if (NR > 1) print $1 }'`
 do
-    vim-cmd vmsvc/power.getstate $VMID
-
     ON=`vim-cmd vmsvc/power.getstate $VMID | grep "Powered on" || EXIT=$?`
     if [[ -n "$ON" ]]; then
         HB=`vim-cmd vmsvc/get.guestheartbeatStatus $VMID`
         if [ "$HB" != "green" ]; then
-            UPTIME=`vim-cmd vmsvc/get.summary 259 | sed -rn 's/\ +uptimeSeconds\ =\ ([0-9]+),\ +/\1/p'`
+            UPTIME=`vim-cmd vmsvc/get.summary $VMID | sed -rn 's/\ +uptimeSeconds\ =\ ([0-9]+),\ +/\1/p'`
             if [ "$UPTIME" -gt "$GRACE_PERIOD" ]; then
                 echo "Powering off VM: $VMID"
-                vim-cmd vmsvc/power.off $VMID
+                vim-cmd vmsvc/power.off $VMID > /dev/null
             fi
         fi
     fi
