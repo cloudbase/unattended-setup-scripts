8be6d6aedf89b87aec110143a22892a09636e0f6
Alessandro Pilotti <ap@pilotti.it>
2013-10-08 00:11:29 +0300

    Updates RDO configuration script

--- configure-rdo.sh
+++ configure-rdo.sh
@@ -10,50 +10,112 @@ RDO_ADMIN_PASSWORD=Passw0rd
 
 ANSWERS_FILE=packstack_answers.conf
 
+DOMAIN=localdomain
+
+MAX_WAIT_SECONDS=600
+
 BASEDIR=$(dirname $0)
 
-read CONTROLLER_VM_IP NETWORK_VM_IP QEMU_COMPUTE_VM_IP HYPERV_COMPUTE_VM_IP <<< `ssh $ESXI_USER@$ESXI_HOST "cat $IPS_FILE_NAME" | perl -n -e'/rdotest3_[a-z_]+:(.+\n)/ && print $1'`
+read CONTROLLER_VM_NAME CONTROLLER_VM_IP NETWORK_VM_NAME NETWORK_VM_IP QEMU_COMPUTE_VM_NAME QEMU_COMPUTE_VM_IP HYPERV_COMPUTE_VM_NAME HYPERV_COMPUTE_VM_IP <<< `ssh $ESXI_USER@$ESXI_HOST "cat $IPS_FILE_NAME" | perl -n -e'/^(.+)\:(.+)$/ && print "$1\n$2\n"'`
 
 SSH_KEY_FILE=`mktemp -u /tmp/rdo.XXXXXX`
 SSH_KEY_FILE_PUB=$SSH_KEY_FILE.pub
 ssh-keygen -q -t rsa -f $SSH_KEY_FILE -N "" -b 4096
 
-echo "Configuring SSH publick key authetication on the RDO hosts"
+wait_for_listening_port () {
+    HOST=$1
+    PORT=$2
+    TIMEOUT=$3
+    nc -z -w$TIMEOUT $HOST $PORT
+}
 
 configure_ssh_pubkey_auth () {
     HOST=$1
     ssh-keygen -R $HOST
+
+    wait_for_listening_port $HOST 22 $MAX_WAIT_SECONDS
     $BASEDIR/scppass.sh $SSH_KEY_FILE_PUB $RDO_ADMIN@$HOST:$SSH_KEY_FILE_PUB "$RDO_ADMIN_PASSWORD"
     $BASEDIR/sshpass.sh $RDO_ADMIN@$HOST "$RDO_ADMIN_PASSWORD" "mkdir -p ~/.ssh && chmod 700 ~/.ssh && cat $SSH_KEY_FILE_PUB >> ~/.ssh/authorized_keys && chmod 600 ~/.ssh/authorized_keys && restorecon -R -v ~/.ssh"
 }
+
+echo "Configuring SSH public key authentication on the RDO hosts"
 
 configure_ssh_pubkey_auth $CONTROLLER_VM_IP
 configure_ssh_pubkey_auth $NETWORK_VM_IP
 configure_ssh_pubkey_auth $QEMU_COMPUTE_VM_IP
 
-#ssh-keygen -R $CONTROLLER_VM_IP
-#$BASEDIR/scppass.sh $SSH_KEY_FILE_PUB $RDO_ADMIN@$CONTROLLER_VM_IP:$SSH_KEY_FILE_PUB "$RDO_ADMIN_PASSWORD"
-#$BASEDIR/sshpass.sh $RDO_ADMIN@$CONTROLLER_VM_IP "$RDO_ADMIN_PASSWORD" "mkdir -p ~/.ssh && chmod 700 ~/.ssh && cat $SSH_KEY_FILE_PUB >> ~/.ssh/authorized_keys && chmod 600 ~/.ssh/authorized_keys && restorecon -R -v ~/.ssh"
+run_ssh_cmd () {
+    SSHUSER_HOST=$1
+    CMD=$2
+    ssh -i $SSH_KEY_FILE $SSHUSER_HOST -o 'PasswordAuthentication no' "$CMD"
+}
 
-#ssh-keygen -R $NETWORK_VM_IP
-#$BASEDIR/scppass.sh $SSH_KEY_FILE_PUB $RDO_ADMIN@$NETWORK_VM_IP:$SSH_KEY_FILE_PUB "$RDO_ADMIN_PASSWORD"
-#$BASEDIR/sshpass.sh $RDO_ADMIN@$NETWORK_VM_IP "$RDO_ADMIN_PASSWORD" "mkdir -p ~/.ssh && chmod 700 ~/.ssh && cat $SSH_KEY_FILE_PUB >> ~/.ssh/authorized_keys && chmod 600 ~/.ssh/authorized_keys && restorecon -R -v ~/.ssh"
+run_ssh_cmd_with_retry () {
+    SSHUSER_HOST=$1
+    CMD=$2
+    MAX_RETRIES=10
 
-#ssh-keygen -R $QEMU_COMPUTE_VM_IP
-#$BASEDIR/scppass.sh $SSH_KEY_FILE_PUB $RDO_ADMIN@$QEMU_COMPUTE_VM_IP:$SSH_KEY_FILE_PUB "$RDO_ADMIN_PASSWORD"
-#$BASEDIR/sshpass.sh $RDO_ADMIN@$QEMU_COMPUTE_VM_IP "$RDO_ADMIN_PASSWORD" "mkdir -p ~/.ssh && chmod 700 ~/.ssh && cat $SSH_KEY_FILE_PUB >> ~/.ssh/authorized_keys && chmod 600 ~/.ssh/authorized_keys && restorecon -R -v ~/.ssh"
+    COUNTER=0
+    while [ $COUNTER -lt $MAX_RETRIES ]; do
+        EXIT=0
+        run_ssh_cmd $SSHUSER_HOST "$CMD" || EXIT=$?
+        if [ $EXIT -eq 0 ]; then
+            return 0
+        fi
+        let COUNTER=COUNTER+1
+    done
+    return $EXIT
+}
 
-echo "Installing RDO RPMs"
+config_openstack_network_adapter () {
+    SSHUSER_HOST=$1
+    ADAPTER=$2    
 
-ssh -i $SSH_KEY_FILE $RDO_ADMIN@$CONTROLLER_VM_IP -o 'PasswordAuthentication no' "yum install -y http://rdo.fedorapeople.org/openstack/openstack-grizzly/rdo-release-grizzly.rpm & yum install -y openstack-packstack && yum install -y openstack-utils"
+    run_ssh_cmd_with_retry $SSHUSER_HOST "cat << EOF > /etc/sysconfig/network-scripts/ifcfg-$ADAPTER
+DEVICE="$ADAPTER"
+BOOTPROTO="none"
+MTU="1500"
+ONBOOT="yes"
+EOF"
+
+    run_ssh_cmd_with_retry $SSHUSER_HOST "ifup $ADAPTER"
+}
+
+set_hostname () {
+    SSHUSER_HOST=$1
+    FQDN=$2
+    IP=$3
+    HOSTNAME=${FQDN%%.*}
+
+    run_ssh_cmd_with_retry $SSHUSER_HOST "sed -i 's/^HOSTNAME=.\+$/HOSTNAME=$FQDN/g' /etc/sysconfig/network"
+    run_ssh_cmd_with_retry $SSHUSER_HOST "sed -r '/$FQDN/d' -i /etc/hosts && echo '$IP $HOSTNAME $FQDN' >> /etc/hosts"
+    run_ssh_cmd_with_retry $SSHUSER_HOST "hostname $FQDN"
+    run_ssh_cmd_with_retry $SSHUSER_HOST "service network restart"
+}
+
+echo "Configuring networking"
+
+set_hostname $RDO_ADMIN@$CONTROLLER_VM_IP $CONTROLLER_VM_NAME.$DOMAIN $CONTROLLER_VM_IP
+
+config_openstack_network_adapter $RDO_ADMIN@$NETWORK_VM_IP eth1
+config_openstack_network_adapter $RDO_ADMIN@$NETWORK_VM_IP eth2
+set_hostname $RDO_ADMIN@$NETWORK_VM_IP $NETWORK_VM_NAME.$DOMAIN $NETWORK_VM_IP
+
+config_openstack_network_adapter $RDO_ADMIN@$QEMU_COMPUTE_VM_IP eth1
+set_hostname $RDO_ADMIN@$QEMU_COMPUTE_VM_IP $QEMU_COMPUTE_VM_NAME.$DOMAIN $QEMU_COMPUTE_VM_IP
+
+echo "Installing RDO RPMs on controller"
+
+run_ssh_cmd_with_retry $RDO_ADMIN@$CONTROLLER_VM_IP "yum install -y http://rdo.fedorapeople.org/openstack/openstack-grizzly/rdo-release-grizzly.rpm || EXIT=$?"
+run_ssh_cmd_with_retry $RDO_ADMIN@$CONTROLLER_VM_IP "yum install -y openstack-packstack && yum install -y openstack-utils"
 
 echo "Generating Packstack answer file"
 
-ssh -i $SSH_KEY_FILE $RDO_ADMIN@$CONTROLLER_VM_IP -o 'PasswordAuthentication no' "packstack --gen-answer-file=$ANSWERS_FILE"
+run_ssh_cmd_with_retry $RDO_ADMIN@$CONTROLLER_VM_IP "packstack --gen-answer-file=$ANSWERS_FILE"
 
 echo "Configuring Packstack answer file"
 
-ssh -i $SSH_KEY_FILE $RDO_ADMIN@$CONTROLLER_VM_IP -o 'PasswordAuthentication no' "\
+run_ssh_cmd_with_retry $RDO_ADMIN@$CONTROLLER_VM_IP "\
 openstack-config --set $ANSWERS_FILE general CONFIG_SSH_KEY /root/.ssh/id_rsa.pub && \
 openstack-config --set $ANSWERS_FILE general CONFIG_NTP_SERVERS 0.pool.ntp.org,1.pool.ntp.org,2.pool.ntp.org,3.pool.ntp.org && \
 openstack-config --set $ANSWERS_FILE general CONFIG_CINDER_VOLUMES_SIZE 20G && \
@@ -74,5 +136,36 @@ scp -i $SSH_KEY_FILE -o 'PasswordAuthentication no' $SSH_KEY_FILE_PUB $RDO_ADMIN
 
 echo "Running Packstack"
 
-ssh -i $SSH_KEY_FILE $RDO_ADMIN@$CONTROLLER_VM_IP -o 'PasswordAuthentication no' "packstack --answer-file=$ANSWERS_FILE"
+run_ssh_cmd_with_retry $RDO_ADMIN@$CONTROLLER_VM_IP "packstack --answer-file=$ANSWERS_FILE"
+
+echo "Enabling Quantum firewall driver on controller"
+run_ssh_cmd_with_retry $RDO_ADMIN@$CONTROLLER_VM_IP "sed -i 's/^#\ firewall_driver/firewall_driver/g' /etc/quantum/plugins/openvswitch/ovs_quantum_plugin.ini && service quantum-server restart"
+
+echo "Applying additional OVS configuration on $NETWORK_VM_IP"
+
+run_ssh_cmd_with_retry $RDO_ADMIN@$NETWORK_VM_IP "ovs-vsctl list-ports br-ex | grep eth2 || ovs-vsctl add-port br-ex eth2"
+
+install_3x_kernel () {
+    SSHUSER_HOST=$1
+    run_ssh_cmd_with_retry $SSHUSER_HOST "yum install -y centos-release-xen && yum update -y --disablerepo=* --enablerepo=Xen4CentOS kernel" 
+}
+
+echo "Installing 3.x kernel on network and compute nodes"
+
+install_3x_kernel $RDO_ADMIN@$NETWORK_VM_IP
+install_3x_kernel $RDO_ADMIN@$QEMU_COMPUTE_VM_IP
+
+echo "Rebooting nodes to load the new kernel"
+
+run_ssh_cmd_with_retry $RDO_ADMIN@$CONTROLLER_VM_IP reboot
+run_ssh_cmd_with_retry $RDO_ADMIN@$NETWORK_VM_IP reboot
+run_ssh_cmd_with_retry $RDO_ADMIN@$QEMU_COMPUTE_VM_IP reboot
+
+echo "Validating configuration"
+
+wait_for_listening_port $CONTROLLER_VM_IP 22 $MAX_WAIT_SECONDS
+
+run_ssh_cmd_with_retry $RDO_ADMIN@$CONTROLLER_VM_IP "source ./keystonerc_admin && nova service-list | sed -e '$d' | awk '(NR > 3) {print $10}' | sed -rn '/down/q1'"
+
+run_ssh_cmd_with_retry $RDO_ADMIN@$CONTROLLER_VM_IP "source ./keystonerc_admin && quantum agent-list -f csv | sed -e '1d' | sed -rn 's/".*",".*",".*","(.*)",.*/\1/p | sed '/xxx/q1'"
 
