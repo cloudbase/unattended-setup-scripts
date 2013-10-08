40bf96cdb523f62498cda7fd1b577a0b2ade7c16
Alessandro Pilotti <ap@pilotti.it>
2013-10-08 23:48:59 +0300

    Adds portgroups and vswithes to delete-rdo.sh

--- delete-rdo.sh
+++ delete-rdo.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 set -e
 
-if [ $# -ne 2 ]; then
-    echo "Usage: $0 <datastore> <rdo_name>"
+if [ $# -ne 3 ]; then
+    echo "Usage: $0 <datastore> <rdo_name> <esxi_public_switch>"
     exit 1
 fi
 
 DATASTORE=$1
 RDO_NAME=$2
+EXT_SWITCH=$3
 
 MGMT_NETWORK="$RDO_NAME"_mgmt
 DATA_NETWORK="$RDO_NAME"_data
@@ -27,5 +28,16 @@ $BASEDIR/delete-esxi-vm.sh "$NETWORK_VM_NAME" "$DATASTORE"
 $BASEDIR/delete-esxi-vm.sh "$QEMU_COMPUTE_VM_NAME" "$DATASTORE"
 $BASEDIR/delete-esxi-vm.sh "$HYPERV_COMPUTE_VM_NAME" "$DATASTORE"
 
+echo "Removing $POOL_NAME resource pool"
 $BASEDIR/delete-esxi-resource-pool.sh "$POOL_NAME"
+
+echo "Removing $MGMT_NETWORK portgroup"
+vim-cmd hostsvc/net/portgroup_remove "$EXT_SWITCH" "$MGMT_NETWORK"
+echo "Removing $EXT_NETWORK portgroup"
+vim-cmd hostsvc/net/portgroup_remove "$EXT_SWITCH" "$EXT_NETWORK"
+
+echo "Removing $DATA_NETWORK portgroup"
+vim-cmd hostsvc/net/portgroup_remove "$DATA_NETWORK" "$DATA_NETWORK"
+echo "Removing $DATA_NETWORK switch"
+vim-cmd hostsvc/net/vswitch_remove "$DATA_NETWORK"
 
