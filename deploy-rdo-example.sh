24a74055f525ec222b5ac93c870a84053594a925
Alessandro Pilotti <ap@pilotti.it>
2013-10-15 20:04:44 +0300

    Uses cloned vmkds on RDO by default

--- deploy-rdo-example.sh
+++ deploy-rdo-example.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+VMDK_OPTION=$1
+
 RDO_NAME=rdo-test-$RANDOM
 ESXI_HOST=10.7.2.5
 ESXI_USER=root
@@ -9,8 +11,13 @@ DATASTORE=datastore1
 ESXI_PUBLIC_SWITCH=vSwitch0
 ESXI_PUBLIC_VMNIC=vmnic0
 
-LINUX_TEMPLATE_VMDK=/vmfs/volumes/datastore1/centos-6.4-template-40G/centos-6.4-template-40G-000001.vmdk
-HYPERV_TEMPLATE_VMDK=/vmfs/volumes/datastore1/hyperv-2012-template-80G/hyperv-2012-template-80G-000001.vmdk
+LINUX_TEMPLATE_VMDK=/vmfs/volumes/datastore1/centos-6.4-template-40G/centos-6.4-template-40G.vmdk
+HYPERV_TEMPLATE_VMDK=/vmfs/volumes/datastore1/hyperv-2012-template-80G/hyperv-2012-template-80G.vmdk
+
+if [ "$VMDK_OPTION" == "use_linked_vmdks" ]; then
+    LINUX_TEMPLATE_VMDK=${LINUX_TEMPLATE_VMDK%%.vmdk}-000001.vmdk
+    HYPERV_TEMPLATE_VMDK=${HYPERV_TEMPLATE_VMDK%%.vmdk}-000001.vmdk    
+fi
 
 BASEDIR=$(dirname $0)
 
