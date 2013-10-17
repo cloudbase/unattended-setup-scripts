161ce8c250f96e029797015f2df9fec65889c053
Gabriel Samfira <samfiragabriel@gmail.com>
2013-10-18 01:02:16 +0300

    Added fix for PAM authentication
    * Added replace in place for users with /bin/sh in /etc/security/access.conf
    * added cronjob that executes /etc/rc.local.d/local.sh

---
+++ esxi-fix-pam-ssh-auth.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+grep '^#PAMFix' /etc/rc.local.d/local.sh
+
+if [ $? != 0 ]
+then
+    sed -i '/exit 0/d' /etc/rc.local.d/local.sh
+    cat <<EOF >> /etc/rc.local.d/local.sh
+
+for i in \$(grep "/bin/sh" /etc/passwd | cut -f1 -d":")
+do
+    /bin/sed -i 's/^-:'\$i':/+:'\$i':/g' /etc/security/access.conf
+done
+
+grep local.sh /var/spool/cron/crontabs/root > /dev/null 2>&1
+if [ \$? != 0 ]
+then
+    echo '*    *    *   *   *   /etc/rc.local.d/local.sh' >> /var/spool/cron/crontabs/root
+fi
+#PAMFix
+EOF
+fi
+
+/etc/rc.local.d/local.sh
+/sbin/auto-backup.sh
