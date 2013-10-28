99a67245617b1ffda80b92c3b7457fe038b9f38a
Octavian Ciuhandu <ociuhandu@cloudbasesolutions.com>
2013-10-29 01:30:53 +0200

    Added "InstallCloudbaseInit" switch to the script
    One can select if Cloudbase-Init is also installed by adding -InstallCloudbaseInit option.

--- UpdateAndSysprep.ps1
+++ UpdateAndSysprep.ps1
@@ -1,3 +1,6 @@
+Param(
+  [switch]$InstallCloudbaseInit
+)
 $ErrorActionPreference = "Stop"
 
 Get-WUInstall -AcceptAll -IgnoreReboot
@@ -9,6 +12,13 @@ else
 {
     Remove-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" -Name Unattend*
     Remove-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name AutoLogonCount
-    Start-Process -FilePath msiexec -ArgumentList "/i C:\Windows\Temp\CloudbaseInitSetup.msi /qn /l*v C:\Windows\Temp\CloudbaseInitSetup_Beta.log" -Wait
-    C:\Windows\System32\Sysprep\Sysprep.exe /generalize /oobe /shutdown /unattend:C:\Windows\Temp\Unattend.xml
+    if($InstallCloudbaseInit)
+    {
+        Start-Process -FilePath msiexec -ArgumentList "/i C:\Windows\Temp\CloudbaseInitSetup.msi /qn /l*v C:\Windows\Temp\CloudbaseInitSetup_Beta.log" -Wait
+        C:\Windows\System32\Sysprep\Sysprep.exe /generalize /oobe /shutdown /unattend:C:\Program\ Files\ (x86)\Cloudbase\ Solutions\Cloudbase-Init\conf\Unattend.xml
+    }
+    else
+    {
+        C:\Windows\System32\Sysprep\Sysprep.exe /generalize /oobe /shutdown /unattend:C:\Windows\Temp\Unattend.xml
+    }
 }
