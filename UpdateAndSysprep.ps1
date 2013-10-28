3d98c2313dff6f6c00d0b8c34a9b3fc001cc888f
Octavian Ciuhandu <ociuhandu@cloudbasesolutions.com>
2013-10-28 23:09:17 +0200

    Added Cloudbase-Init right before final sysprep

--- UpdateAndSysprep.ps1
+++ UpdateAndSysprep.ps1
@@ -9,5 +9,6 @@ else
 {
     Remove-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" -Name Unattend*
     Remove-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name AutoLogonCount
+    msiexec /i C:\Windows\Temp\CloudbaseInitSetup.msi /qn /l*v C:\Windows\Temp\CloudbaseInitSetup_Beta.log
     C:\Windows\System32\Sysprep\Sysprep.exe /generalize /oobe /shutdown /unattend:C:\Windows\Temp\Unattend.xml
 }
