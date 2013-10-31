2f321a2a7be1a12a36465283d7158db999227596
Alessandro Pilotti <apilotti@cloudbasesolutions.com>
2013-10-31 03:14:03 +0200

    Update UpdateAndSysprep.ps1

--- UpdateAndSysprep.ps1
+++ UpdateAndSysprep.ps1
@@ -3,9 +3,13 @@ Param(
 )
 $ErrorActionPreference = "Stop"
 
+$Host.UI.RawUI.WindowTitle = "Installing updates..."
+
 Get-WUInstall -AcceptAll -IgnoreReboot
 if (Get-WURebootStatus -Silent)
 {
+    $Host.UI.RawUI.WindowTitle = "Updates installed. Rebooting."
+  
     shutdown /r /t 0
 }
 else
@@ -14,15 +18,23 @@ else
     Remove-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name AutoLogonCount
     if($InstallCloudbaseInit)
     {
+        $Host.UI.RawUI.WindowTitle = "Downloading Cloudbase-Init..."
+      
         Invoke-WebRequest -Uri http://www.cloudbase.it/downloads/CloudbaseInitSetup_Beta.msi -OutFile C:\Windows\Temp\CloudbaseInitSetup_Beta.msi
-        Write-Host "Sleep 60 seconds"
-        Start-Sleep -s 60
-        Write-Host "Install Cloudbase-Init"
-        Start-Process -FilePath msiexec -ArgumentList "/i C:\Windows\Temp\CloudbaseInitSetup.msi /qn /l*v C:\Windows\Temp\CloudbaseInitSetup_Beta.log" -Wait
-        Write-Host "Sleep 60 seconds take 2"
-        Start-Sleep -s 60
-        Write-Host "Install Cloudbase-Init take 2"
-        Start-Process -FilePath msiexec -ArgumentList "/i C:\Windows\Temp\CloudbaseInitSetup.msi /qn /l*v C:\Windows\Temp\CloudbaseInitSetup_Beta2.log" -Wait
+        
+        $Host.UI.RawUI.WindowTitle = "Installing Cloudbase-Init..."
+                
+        do 
+        {
+            $p = Start-Process -Wait -PassThru -FilePath msiexec -ArgumentList "/i C:\Windows\Temp\CloudbaseInitSetup.msi /qn /l*v C:\Windows\Temp\CloudbaseInitSetup_Beta.log"
+            if ($p.ExitCode -ne 0)
+            {
+                Write-Host "Cloudbase-Init setup failed. Retrying after a short break."
+                Start-Sleep -s 30
+            }
+        }
+        while($p.ExitCode -ne 0)
+        
         C:\Windows\System32\Sysprep\Sysprep.exe /generalize /oobe /shutdown /unattend:C:\Program\ Files\ (x86)\Cloudbase\ Solutions\Cloudbase-Init\conf\Unattend.xml
     }
     else
