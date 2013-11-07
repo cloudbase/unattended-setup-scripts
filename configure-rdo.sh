310747e3e79a24980acaa7c383c34298bc357214
Alessandro Pilotti <ap@pilotti.it>
2013-11-07 14:06:56 +0800

    Open Glance port for Hyper-V node

--- configure-rdo.sh
+++ configure-rdo.sh
@@ -178,6 +178,7 @@ run_ssh_cmd_with_retry $RDO_ADMIN@$CONTROLLER_VM_IP "iptables -I INPUT -s $NETWO
 
 if [ -n "$HYPERV_COMPUTE_VM_IP" ]; then
     run_ssh_cmd_with_retry $RDO_ADMIN@$CONTROLLER_VM_IP "iptables -I INPUT -s $HYPERV_COMPUTE_VM_IP/32 -p tcp --dport 9696 -j ACCEPT"
+    run_ssh_cmd_with_retry $RDO_ADMIN@$CONTROLLER_VM_IP "iptables -I INPUT -s $HYPERV_COMPUTE_VM_IP/32 -p tcp --dport 9292 -j ACCEPT"
 fi
 
 run_ssh_cmd_with_retry $RDO_ADMIN@$CONTROLLER_VM_IP "service iptables save"
