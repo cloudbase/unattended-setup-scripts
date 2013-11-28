4d80fb7ed26026dc30c6fa24629b5271355bf7da
Alessandro Pilotti <ap@pilotti.it>
2013-11-28 03:03:31 +0200

    Updates the default datastore to ssd1

--- create-centos-64-vm-template.sh
+++ create-centos-64-vm-template.sh
@@ -2,7 +2,7 @@
 BASEDIR=$(dirname $0)
 
 FLOPPY_IMAGE_PATH=$BASEDIR/ks.flp
-DATASTORE=datastore1
+DATASTORE=ssd1
 
 $BASEDIR/create-vm-template.sh $DATASTORE rhel6-64 centos-6.4-template-40G 1024 40G /vmfs/volumes/datastore2/iso/CentOS-6.4-x86_64-bin-DVD1-ks.iso linux $FLOPPY_IMAGE_PATH "VM Network" true true 
 
--- create-hyperv-2012-vm-template.sh
+++ create-hyperv-2012-vm-template.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 BASEDIR=$(dirname $0)
-DATASTORE=datastore1
+DATASTORE=ssd1
 FLOPPY_IMAGE_PATH=$BASEDIR/unattend_hyperv_2012_cloudbaseinit.flp
 ISO_PATH=/vmfs/volumes/datastore2/iso/en_microsoft_hyper-v_server_2012_x64_dvd_915600.iso
 
