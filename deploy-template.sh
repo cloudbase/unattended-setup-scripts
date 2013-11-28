06daf5563037a3e926a1420876afdb68941046dc
Alessandro Pilotti <ap@pilotti.it>
2013-11-28 18:19:38 +0200

    Datastore naming fixes

--- deploy-template.sh
+++ deploy-template.sh
@@ -2,7 +2,7 @@
 set -e
 
 if [ $# -lt 3 ]; then
-    echo "Usage: $0 <esxi_user> <esxi_host> <template> [<scripts_datastore>] [<resource_pool_name>] [<datastore>] [<vm_name>] [<ram>] [<network>]"
+    echo "Usage: $0 <esxi_user> <esxi_host> <template> [<scripts_datastore>] [<template_datastore>] [<datastore>] [<resource_pool_name>] [<vm_name>] [<ram>] [<network>]"
     exit 1
 fi
 
@@ -10,17 +10,16 @@ ESXI_USER=$1
 ESXI_HOST=$2
 TEMPLATE_NAME=$3
 SCRIPTS_DATASTORE=${4:-ssd1}
-POOL_NAME=${5:-$ESXI_USER}
+TEMPLATE_DATASTORE=${5:-ssd1}
 DATASTORE=${6:-ssd1}
-VM_NAME=${7:-$TEMPLATE_NAME-$RANDOM}
-RAM=${8:-1024}
-NETWORK=${9:-"VM Network"}
-
-TEMPLATE_DATASTORE=ssd1
+POOL_NAME=${7:-$ESXI_USER}
+VM_NAME=${8:-$TEMPLATE_NAME-$RANDOM}
+RAM=${9:-1024}
+NETWORK=${10:-"VM Network"}
 
 NET_ADAPTER_TYPE=vmxnet3
 
-ESXI_BASEDIR=/vmfs/volumes/ssd1/unattended-scripts
+ESXI_BASEDIR=/vmfs/volumes/$SCRIPTS_DATASTORE/unattended-scripts
 TEMPLATE_BASEDIR=/vmfs/volumes/$TEMPLATE_DATASTORE/$TEMPLATE_NAME
 
 TEMPLATE_VMX_FILE=$TEMPLATE_BASEDIR/$TEMPLATE_NAME.vmx
