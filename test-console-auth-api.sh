2efc34c30c0e75f26bf87427b0a7b97b5e9033e5
Alessandro Pilotti <apilotti@cloudbasesolutions.com>
2014-03-05 21:50:25 +0200

    Updates console auth test script

--- test-console-auth-api.sh
+++ test-console-auth-api.sh
@@ -12,20 +12,16 @@ NOVA_URL=`echo $TOKENS_RESP | python -c "import json; import sys; d=json.load(sy
 echo "Getting RDP console"
 
 CONSOLE_RESP=`curl -s -H "X-Auth-Token: $TOKEN" $NOVA_URL/servers/$INSTANCE_ID/action -X "POST" -H 'Content-type: application/json' -d '{"os-getRDPConsole":{"type":"rdp-html5"}}'`
-#CONSOLE_RESP=`curl -s -H "X-Auth-Token: $TOKEN" $NOVA_URL/servers/$INSTANCE_ID/action -X "POST" -H 'Content-type: application/json' -d '{"os-getVNCConsole":{"type":"novnc"}}'`
-
-echo $CONSOLE_RESP
 
 CONSOLE_URL=`echo $CONSOLE_RESP | python -c "import json; import sys; d=json.load(sys.stdin); print d['console']['url']"`
-echo $CONSOLE_URL
-
 CONSOLE_TOKEN=${CONSOLE_URL#*=}
-
 echo $CONSOLE_TOKEN
 
 echo "Getting console connect info"
 
-GET_CONSOLE_CONN_RESP=`curl -s -H "X-Auth-Token: $TOKEN" $NOVA_URL/console-auth-tokens/$CONSOLE_TOKEN/action -X "POST" -H 'Content-type: application/json' -d '{"os-getConsoleConnectInfo": null}'`
+GET_CONSOLE_CONN_RESP=`curl -s -H "X-Auth-Token: $TOKEN" $NOVA_URL/os-console-auth-tokens/$CONSOLE_TOKEN -X "GET" -H 'Content-type: application/json'`
+
+echo $GET_CONSOLE_CONN_RESP
 
 HOST=`echo $GET_CONSOLE_CONN_RESP | python -c "import json; import sys; d=json.load(sys.stdin); print d['console']['host']"`
 PORT=`echo $GET_CONSOLE_CONN_RESP | python -c "import json; import sys; d=json.load(sys.stdin); print d['console']['port']"`
