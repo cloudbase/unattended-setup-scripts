9059e7a423faa8809f7e4fc2f025b66f0787f495
Alessandro Pilotti <apilotti@cloudbasesolutions.com>
2014-08-28 22:39:00 +0300

    Removes the short path conversion in Python exe launcher script

--- change_python_launchers_exe_path.py
+++ change_python_launchers_exe_path.py
@@ -1,6 +1,5 @@
 import os
 import sys
-import win32api
 
 from pip._vendor.distlib import scripts
 
@@ -8,6 +7,6 @@ specs = 'nova = novaclient.shell:main'
 
 scripts_path = os.path.join(os.path.dirname(sys.executable), 'Scripts')
 m = scripts.ScriptMaker(None, scripts_path)
-m.executable = win32api.GetShortPathName(sys.executable)
+m.executable = sys.executable
 m.make(specs)
 
