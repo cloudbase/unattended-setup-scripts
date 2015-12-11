b5d66fdd24e97627db5f8c132d0e8cd67de0ffe3
Alessandro Pilotti <apilotti@cloudbasesolutions.com>
2015-12-11 19:36:20 +0200

    Adds stdin support in pasteopenstack.py

--- pasteopenstack.py
+++ pasteopenstack.py
@@ -2,13 +2,15 @@
 import sys
 from xmlrpclib import ServerProxy
 
-filename = sys.argv[1]
-
-with open(filename, 'rb') as f:
-    text = f.read()
+if len(sys.argv) > 1:
+    filename = sys.argv[1]
+    with open(filename, 'rb') as f:
+        text = f.read()
+else:
+    filename = 'stdin'
+    text = sys.stdin.read()
 
 s = ServerProxy('http://paste.openstack.org/xmlrpc/', allow_none=True);
 id = s.pastes.newPaste(None, text, None, filename)
 
 print 'http://paste.openstack.org/show/%s/' % id
-
