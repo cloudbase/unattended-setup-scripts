4a4e45b1660b37dfb7a7a21daa2bc163d37bb75d
Alessandro Pilotti <apilotti@cloudbasesolutions.com>
2016-02-27 19:31:39 +0200

    pasteopenstack.py Python 3 compatibility

--- pasteopenstack.py
+++ pasteopenstack.py
@@ -13,4 +13,4 @@ else:
 s = ServerProxy('http://paste.openstack.org/xmlrpc/', allow_none=True);
 id = s.pastes.newPaste(None, text, None, filename)
 
-print 'http://paste.openstack.org/show/%s/' % id
+print('http://paste.openstack.org/show/%s/' % id)
